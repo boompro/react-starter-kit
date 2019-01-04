968ac40511827c7a46788735a09a752a4bea0456
SuhushinAS <a.suhushin@dev.userstory.ru>
2019-01-05 00:19:49 +0700

    fix

---
+++ www/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'cache_v1';
+const urlsToCache = [
+    '/',
+    '/api/v1/config',
+    '/api/v1/example',
+    '/build/common.min.js',
+    '/build/index.min.css',
+    '/build/index.min.js',
+    '/favicon.ico',
+    '/index.html',
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('activate', (e) => {
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((response) => {
+        if (response) {
+            return response;
+        }
+
+        return fetch(e.request)
+    }));
+});
